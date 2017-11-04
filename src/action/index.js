7a029285a2c1fc564aa65151f1362d0c61a3d422
kevin hu <hu.kevin.w@gmail.com>
2017-11-04 19:01:19 -0400

    Create action, index.js

---
+++ src/action/index.js
@@ -0,0 +1,15 @@
+export const voteReact = () => {
+    return {
+        type: 'VORT_REACT'
+    }
+}
+export const voteAngular = () => {
+    return {
+        type: 'VORT_ANGULAR'
+    }
+}
+export const voteVuejs = () => {
+    return {
+        type: 'VORT_VUEJS'
+    }
+}
